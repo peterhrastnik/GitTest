03f3832404387c51544c96d8cf0654bc497cd666
peterhrastnik <peter@hrastnik.at>
2013-06-12 12:54:00 +0200

    2nd commit

---
+++ src/at/hrastnik/ATestClass.java
@@ -0,0 +1,14 @@
+package at.hrastnik;
+
+
+public class ATestClass {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
