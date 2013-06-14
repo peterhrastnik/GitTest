6ce808b0f210d1e81524f8688dc2a4876120bd6c
peterhrastnik <peter@hrastnik.at>
2013-06-14 10:11:45 +0200

    new output

--- src/at/hrastnik/ATestClass.java
+++ src/at/hrastnik/ATestClass.java
@@ -8,7 +8,7 @@ public class ATestClass {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("STARTING ...");
+        System.out.println("STARTING NEW ...");
 
     }
 
