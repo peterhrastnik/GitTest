2eae3f3af5c423c1669a68fe16d713a3c740a533
peterhrastnik <peter@hrastnik.at>
2013-06-14 10:41:42 +0200

    added in master

---
+++ src/at/hrastnik/NewClassInMaster.java
@@ -0,0 +1,14 @@
+package at.hrastnik;
+
+
+public class NewClassInMaster {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
