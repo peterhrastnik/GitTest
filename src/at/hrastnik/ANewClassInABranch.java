89fa2798fee44adffcc87ce7afb6db58965fc046
peterhrastnik <peter@hrastnik.at>
2013-06-14 10:45:53 +0200

    merged from firstBranch

---
+++ src/at/hrastnik/ANewClassInABranch.java
@@ -0,0 +1,14 @@
+package at.hrastnik;
+
+
+public class ANewClassInABranch {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
